608f8445cf35970f9f8e3ca6eb267f415eca7e77
Jerick Signapan <jericksignapan3331@gmail.com>
2025-10-06 14:40:54 +0800

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./modules/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+    title: 'Login',
+    // canActivate: [
+    //   () => import('./core/guards/hastoken.guard').then((m) => m.hastokenGuard),
+    // ],
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  //   {
+  //     path: 'home',
+  //     loadComponent: () =>
+  //       import('./modules/home/home.component').then((m) => m.HomeComponent),
+  //     canActivate: [
+  //       () =>
+  //         import('./core/guards/is-authenticated.guard').then(
+  //           (m) => m.isAuthenticatedGuard
+  //         ),
+  //     ],
+  //     children: homeRoute,
+  //   },
+];
